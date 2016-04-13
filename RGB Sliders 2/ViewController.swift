f955325909ce98ec08747861f218c69b30a698cd
Brannen Clark <bclark@bclark-2017.local>
2016-04-13 14:16:53 -0400

    Initial Commit

---
+++ RGB Sliders 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB Sliders 2
+//
+//  Created by Brannen Clark on 4/13/16.
+//  Copyright © 2016 Thinkful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
