7d6d0981d30ff4e29e4e8ee01914b96ed4b95414
Brannen Clark <bclark@bclark-2017.local>
2016-04-18 13:33:02 -0400

    newest version

--- RGB Sliders 2/ViewController.swift
+++ RGB Sliders 2/ViewController.swift
@@ -9,31 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var colorSquare: UIView!
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
         colorSquare.layer.borderColor = UIColor.blackColor().CGColor
         colorSquare.layer.borderWidth = 1
         let defaults = NSUserDefaults.standardUserDefaults()
         redSlider.value = defaults.floatForKey("red")
         greenSlider.value = defaults.floatForKey("green")
         blueSlider.value = defaults.floatForKey("blue")
-        
-    
-    
-    
-    
-    
-    
-    
+        colorSquare.backgroundColor = UIColor (red: red, green: green, blue: blue, alpha: 1)
     }
     
-   
-
     
     
     
@@ -52,17 +47,16 @@ class ViewController: UIViewController {
         defaults.setFloat(redSlider.value, forKey: "red")   //2
         defaults.setFloat(greenSlider.value, forKey: "green")
         defaults.setFloat(blueSlider.value, forKey: "blue")
-        defaults.synchronize()
+        defaults.synchronize()    //3
     
-    }
-    
+        
+            }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if (segue.identifier == "openColor") {
             let newViewController = segue.destinationViewController
             newViewController.view.backgroundColor = colorSquare.backgroundColor
         }
     }
-
 
 
 
